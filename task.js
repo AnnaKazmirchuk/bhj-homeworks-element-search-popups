da4a950250a871a606eee74cc42ecfdb1ad05b11
Anna Kazmirchuk <93005766+AnnaKazmirchuk@users.noreply.github.com>
2022-09-20 21:30:04 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,20 @@
+const modal_main = document.querySelector('#modal_main');
+const modal__close = document.querySelectorAll('div.modal__close');
+
+const show_success = document.querySelector('.show-success');
+const modal_success = document.querySelector('#modal_success');
+
+modal_main.className = 'modal modal_active';
+
+modal__close[0].onclick = () => {
+	modal_main.className = 'modal'
+}
+show_success.onclick = () => {
+	modal_main.className = 'modal'
+	modal_success.className = 'modal modal_active';
+}
+modal__close[1].onclick = () => {
+	modal_success.className = 'modal';
+	modal_main.className = 'modal';
+}
+
